c7d4acd2d9e2c6920af8eaf6f59b6ada6b698e02
tomastk <gonzalotomasdossantos@gmail.com>
2024-06-05 17:38:49 -0300

    [pages] added login/register pages

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "../components";
+
+const page = () => {
+  return (
+    <div className="container">
+      <LoginForm />
+    </div>
+  );
+};
+export default page;
---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "../components";
+
+const page = () => {
+  return (
+    <div className="container">
+      <RegisterForm />
+    </div>
+  );
+};
+export default page;
